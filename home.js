0f39dc962e3e0b65cced8f7700fcfb2b552e4299
roll-raihan <raihanahmednasim@gmail.com>
2025-08-26 09:23:48 +0600

    toggling practice,not connected with payooo

--- home.js
+++ home.js
@@ -1,3 +1,4 @@
+ const validPin = 1234;
 document.getElementById('add-money-btn')
     .addEventListener('click', function (e) {
         e.preventDefault();
@@ -7,9 +8,18 @@ document.getElementById('add-money-btn')
         
         const addAmount = parseInt(document.getElementById('add-amount').value);
 
-        const pinNumber = document.getElementById('pin-Num').value;
-
+        const pinNumber = parseInt(document.getElementById('pin-Num').value);
+        //available balance adding and changing
         const availableBalance = parseInt(document.getElementById('available-balance').innerText);
+        //checking if there is 11 digit
+        if(accountNumber.length < 11){
+            alert('Invalid account number.Please,provide valid account');
+            return;
+        };
+        if(pinNumber !== validPin){
+            alert('Please provide valid pin number');
+            return;
+        }
         const availableBalanceAfterAdd = availableBalance + addAmount;
         document.getElementById('available-balance').innerText = availableBalanceAfterAdd;
     })
